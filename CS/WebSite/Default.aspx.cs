54d8a7bf0b6bff6211ae9424a1cdc434031ccab1
DevExpressExampleBot <fakeEmail>
2022-12-07 15:54:52 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -2,13 +2,13 @@
 using System.Threading;
 
 public partial class _Default : System.Web.UI.Page {
-    protected void ASPxCallbackPanel1_Callback(object sender, DevExpress.Web.ASPxClasses.CallbackEventArgsBase e) {
+    protected void ASPxCallbackPanel1_Callback(object sender, DevExpress.Web.CallbackEventArgsBase e) {
         Thread.Sleep(1000);
     }
-    protected void ASPxCallbackPanel2_Callback(object sender, DevExpress.Web.ASPxClasses.CallbackEventArgsBase e) {
+    protected void ASPxCallbackPanel2_Callback(object sender, DevExpress.Web.CallbackEventArgsBase e) {
         Thread.Sleep(2000);
     }
-    protected void ASPxCallbackPanel3_Callback(object sender, DevExpress.Web.ASPxClasses.CallbackEventArgsBase e) {
+    protected void ASPxCallbackPanel3_Callback(object sender, DevExpress.Web.CallbackEventArgsBase e) {
         Thread.Sleep(3000);
     }
 }
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -4,13 +4,13 @@ Imports System.Threading
 
 Partial Public Class _Default
 	Inherits System.Web.UI.Page
-	Protected Sub ASPxCallbackPanel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+	Protected Sub ASPxCallbackPanel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
 		Thread.Sleep(1000)
 	End Sub
-	Protected Sub ASPxCallbackPanel2_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+	Protected Sub ASPxCallbackPanel2_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
 		Thread.Sleep(2000)
 	End Sub
-	Protected Sub ASPxCallbackPanel3_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+	Protected Sub ASPxCallbackPanel3_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
 		Thread.Sleep(3000)
 	End Sub
 End Class
